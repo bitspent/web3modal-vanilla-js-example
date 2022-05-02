1f84b8c81989012098ff0bca5db13c631f880a8c
₿ in the moment <aro.fady@gmail.com>
2022-05-02 15:08:08 +0300

    Update example.js

--- example.js
+++ example.js
@@ -211,8 +211,8 @@ async function onDisconnect() {
 /**
  * Main entry point.
  */
-window.addEventListener('load', async () => {
+
   init();
   document.querySelector("#btn-connect").addEventListener("click", onConnect);
   document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
-});
+
